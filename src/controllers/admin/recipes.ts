3d46ba9d554868a60b078bc334a00a14accd7d98
kanala revanth <revanth.kanala@saarthipedagogy>
2025-02-18 16:39:18 +0530

    added edit recipe

--- src/controllers/admin/recipes.ts
+++ src/controllers/admin/recipes.ts
@@ -7,6 +7,7 @@ import { IAdminUser, IAdminUserModel } from '../../models/admin/admin-users';
 import { IRestaurant, IRestaurantModel } from '../../models/admin/restaurants';
 import { IRestaurantMenu, IRestaurantMenuModel } from '../../models/admin/menu';
 import { RestaurantMenuActions } from '../../logging/actions/admin/admin-recipe';
+import { jsonObjDiff } from 'json-obj-diff';
 const mongoose = require('mongoose');
 
 const AdminUserModel = model('AdminUser') as IAdminUserModel;
@@ -34,6 +35,7 @@ export const getAdminRecipes: APIController = async (req, res) => {
 					},
 					items: {
 						$push: {
+							id: '$_id',
 							name: '$name',
 							description: '$description',
 							price: '$price',
@@ -77,6 +79,46 @@ export const getAdminRecipes: APIController = async (req, res) => {
 		const errorRes = res.createErrorResponse(errorCode, error?.error, error?.errorCode);
 		operation.errorLog(
 			RestaurantMenuActions.get_recipes(req, {
+				data: req.body,
+				response: createErrorResponseData(errorCode, errorRes),
+				meta: {
+					error: {
+						message: error.message,
+						stack: error.stack,
+					},
+				},
+			}),
+		);
+		throw error;
+	}
+};
+
+export const getAdminRecipe: APIController = async (req, res) => {
+	const operation = new APIOperation();
+	try {
+		const restaurantId = req.params.id;
+		const recipeId = req.params.recipeId;
+
+		const results = await RestaurantMenuModel.findById(recipeId);
+
+		if (!results) {
+			throwError(404);
+		}
+
+		operation.log(
+			RestaurantMenuActions.get_recipe(req, {
+				restaurantMenuId: recipeId,
+				data: req.body,
+				response: createSuccessResponseData(),
+			}),
+		);
+
+		return results;
+	} catch (error) {
+		const errorCode = error?.status || 500;
+		const errorRes = res.createErrorResponse(errorCode, error?.error, error?.errorCode);
+		operation.errorLog(
+			RestaurantMenuActions.get_recipe(req, {
 				data: req.body,
 				response: createErrorResponseData(errorCode, errorRes),
 				meta: {
@@ -148,6 +190,86 @@ export const addAdminRecipe: APIController = async (req, res) => {
 		const errorRes = res.createErrorResponse(errorCode, error?.error, error?.errorCode);
 		operation.errorLog(
 			RestaurantMenuActions.add_recipe(req, {
+				data: req.body,
+				response: createErrorResponseData(errorCode, errorRes),
+				meta: {
+					error: {
+						message: error.message,
+						stack: error.stack,
+					},
+				},
+			}),
+		);
+		throw error;
+	}
+};
+
+export const validateUpdateRecipe: MulterValidatorFn = async (req, res, data, cb) => {
+	if (!data || !data.name || !data.category || !data.price) {
+		const errorCode = 400;
+		const errorRes = res.createErrorResponse(errorCode);
+		res.errorRes(errorCode);
+		cb(false);
+		return;
+	}
+	cb(true);
+};
+
+export const updateAdminRecipe: APIController = async (req, res) => {
+	const operation = new APIOperation();
+	try {
+		let data = req.body;
+		const restaurantId = req.params.id;
+		const id = req.params.recipeId;
+		const files = req.files;
+		const session = req.session;
+
+		const recipe = await RestaurantMenuModel.findById(id).lean();
+
+		if (!recipe || recipe.status === 'deleted') {
+			throwError(404);
+		}
+
+		const oldData = clone(recipe);
+
+		data.price = parseInt(data.price);
+
+		if (files?.length) {
+			for (const file of files) {
+				if (file.fieldname === 'image') {
+					data.image = file.path;
+				}
+			}
+		}
+
+		RestaurantMenuModel.deletePrivateProps(data);
+
+		data.updatedBy = session?.user?._id || null;
+		const updatedRecipe = await RestaurantMenuModel.findByIdAndUpdate(
+			id,
+			{
+				$set: data,
+			},
+			{ new: true },
+		);
+
+		const newData = clone(updatedRecipe.toJSON());
+		const dataDiff = jsonObjDiff(oldData, newData);
+
+		operation.log(
+			RestaurantMenuActions.update_recipe(req, {
+				restaurantMenuId: id.toString(),
+				updatedData: dataDiff,
+				response: createSuccessResponseData(),
+			}),
+		);
+
+		return updatedRecipe;
+	} catch (error) {
+		const errorCode = error?.status || 500;
+		const errorRes = res.createErrorResponse(errorCode, error?.error, error?.errorCode);
+		operation.errorLog(
+			RestaurantMenuActions.update_recipe(req, {
 				data: req.body,
 				response: createErrorResponseData(errorCode, errorRes),
 				meta: {
--- src/routes/v1/admin/recipes.ts
+++ src/routes/v1/admin/recipes.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { multerValidation } from '../../../helpers/multer';
-import { getAdminRecipes, addAdminRecipe, validateAddRecipe } from '../../../controllers/admin/recipes';
+import { getAdminRecipes, getAdminRecipe, addAdminRecipe, validateAddRecipe, validateUpdateRecipe, updateAdminRecipe } from '../../../controllers/admin/recipes';
 
 const adminRecipes = express.Router({ mergeParams: true });
 
 adminRecipes.get('/', (req, res) => res.handle(getAdminRecipes));
+adminRecipes.get('/:recipeId', (req, res) => res.handle(getAdminRecipe));
 adminRecipes.post('/', multerValidation(validateAddRecipe), (req, res) => res.handle(addAdminRecipe));
-// adminRecipes.put('/', multerValidation(validateUpdateRecipe), (req, res) => res.handle(updateAdminRecipe));
+adminRecipes.put('/:recipeId', multerValidation(validateUpdateRecipe), (req, res) => res.handle(updateAdminRecipe));
 // adminRecipes.delete('/', (req, res) => res.handle(deleteAdminRecipe));
 
 export default adminRecipes;
